3316677a84baca35a387a6f7387a6a6bf489b931
disanv <disanv03@gmail.com>
2023-05-01 13:45:16 +0200

    Essential CRUD done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -25,15 +25,26 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
-
+  
   def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @task = Task.find(params[:id])
   end
-
+  
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
 
